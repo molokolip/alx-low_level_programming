2e6e599366a08ebd6029da86a21864fea92c9f8c
molokoli <molokolip@gmail.com>
2022-09-23 04:30:20 -0700

    Pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,3 +10,44 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
+#include "main.h"
+
+
+
+
+
+
+
+
+
+/**
+
+
+* reset_to_98 - ur comment
+
+
+* @n: pointer
+
+
+* Return: void
+
+
+*/
+
+
+
+
+
+
+
+void reset_to_98(int *n)
+
+
+{
+
+
+	*n = 98;
+
+
+}
+

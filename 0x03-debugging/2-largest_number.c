550a0a876775fbb24107f58424e92e8e82634a6d
molokoli <molokolip@gmail.com>
2022-09-23 09:00:51 -0700

    debbugging 03

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,13 +9,13 @@
  */
 
 int largest_number(int a, int b, int c)
-{
-int largest;
-if (a >= b && a >= c)
+{int largest;
+
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (b >= a && b >= c)
+else if (b > c)
 {
 largest = b;
 }
@@ -23,5 +23,7 @@ else
 {
 largest = c;
 }
+
 return (largest);
 }
+

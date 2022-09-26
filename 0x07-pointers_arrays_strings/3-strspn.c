2be7d93bb5eefd87e8bc7af2004a7c5302b93b84
molokoli <molokolip@gmail.com>
2022-09-26 03:27:46 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,46 +1,32 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strspn - gets the length of a prefix substring
- * @s: string to be searched
- * @accept: string to be used
- *
- * Return: number of bytes in the initial segment of s which are part of accept
+ * _strspn - returns number of bytes in the initial sengment s
+ * @s: for the count action
+ * @accept: parameter for char
+ * Return: number of bytes;
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	int match = 0;
+	unsigned int num;
+	int i, j, t;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
-		if (_strchr(accept, s[i]) == NULL)
+		t = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				t = 1;
+		}
+		j = 0;
+		if (t == 0)
 			break;
-		match++;
+		num++;
 		i++;
 	}
-
-	return (match);
-}
-
-/**
- * _strchr - locates a char in a string
- * @s: string to be searched
- * @c: char to be checked
- *
- * Return: pointer to the first occurence of c in s
- */
-
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	for (; s[i] != c && s[i] != '\0'; i++);
-
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+	return (i);
 }
